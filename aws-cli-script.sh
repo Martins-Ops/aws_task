80b857bccd1e6b8a29fc264357b6b2c1551570de
oluwafemi <oluwafemi@DESKTOP-1TB3K83.localdomain>
2023-03-21 06:09:33 +0100

    modified bash script

--- aws-cli-script.sh
+++ aws-cli-script.sh
@@ -1,14 +1,26 @@
-#!/bin/sh
+#!/bin/env bash
+
+ROLENAME1="MyCodePipelineRole"
+TRUSTPOLICYFILE1="codepipeline-iam-trust-policy.json"
+POLICYFILE1="codepipeline-policy-for-cloudformation.json"
+
+ROLENAME2="MyCloudFormationRole"
+TRUSTPOLICYFILE2="cloudformation-iam-trust-policy.json"
+POLICYFILE2="cloudformation-policy-for-s3.json"
+
 
 #IAM Role for CodePipeline
-aws iam create-role --role-name MyCodePipelineRole --assume-role-policy-document file://codepipeline-iam-trust-policy.json
+aws iam create-role --role-name $ROLENAME1 --assume-role-policy-document file://$TRUSTPOLICYFILE1 --output json >> output.json 2>&1
+# Attach Inline Policy to Role 
+aws iam put-role-policy --role-name $ROLENAME1 --policy-name  MyCodePipelinePolicy --policy-document file://$POLICYFILE1 
 
-# Attach Inline Policy to Role MyCodePipelineRole
-aws iam put-role-policy --role-name MyCodePipelineRole --policy-name  MyCodePipelinePolicy --policy-document file://codepipeline-policy-for-cloudformation.json
+echo "IAM role and Policy created: $ROLENAME1"
+
 
 #IAM Role for CloudFormation
-aws iam create-role --role-name MyCloudFormationRole --assume-role-policy-document file://cloudformation-iam-trust-policy.json
+aws iam create-role --role-name $ROLENAME2  --assume-role-policy-document file://$TRUSTPOLICYFILE2 --output json >> output.json 2>&1
 
 # Attach Inline Policy to Role MyCloudFormationRole
-aws iam put-role-policy --role-name MyCloudFormationRole  --policy-name  MyCloudFormationPolicy --policy-document file://cloudformation-policy-for-s3.json policy
+aws iam put-role-policy --role-name $ROLENAME2  --policy-name  MyCloudFormationPolicy --policy-document file://$POLICYFILE2 
 
+echo "IAM role and Policy created: $ROLENAME2"
